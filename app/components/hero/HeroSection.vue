5bbd43cc0fbd2459ddfb87347144fcccfbac748d
mohamedDev69 <mohamedchettah0208@gmail.com>
2025-10-26 16:43:57 +0100

    feat: wip navbar / hero section

--- app/components/hero/HeroSection.vue
+++ app/components/hero/HeroSection.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
 
+import Navbar from "../layouts/Navbar.vue";
 </script>
 
 <template>
-  <section
-      class="relative flex items-center justify-center h-[600px] bg-cover bg-center"
-      style="background-image: url('/img/main/background.png');"
-  >
+  <div class="h-[600px] relative bg-cover bg-center " style="background-image: url('/img/main/background.png');" >
+    <Navbar />
+
     <div class="text-center text-white">
-      <h1 class="text-4xl font-bold mb-4">Ta headline ici</h1>
-      <button class="bg-blue-600 px-4 py-2 rounded">Call to Action</button>
+      <h1 class="text-5xl mb-4" >On ne crée pas des sites. <br> On construit des expériences</h1>
+      <button class="bg-black-light px-4 py-2 rounded">Call to Action</button>
     </div>
-  </section>
+  </div>
 </template>
 
 <style scoped>
---
+++ app/components/layouts/Navbar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const isSticky = ref(false)
+</script>
+
+<template>
+  <!-- Header -->
+  <header class="flex justify-center items-center">
+    <!-- Logo -->
+    <NuxtLink to="/" class="mt-10 flex justify-center items-center">
+      <NuxtImg src="/img/main/logo.svg"  alt="MC Studio" class="h-16" />
+    </NuxtLink>
+
+    <!-- Nav desktop -->
+    <nav class="hidden sm:flex items-center gap-2 sm:gap-4">
+      <NuxtLink :class="isSticky ? 'text-beige hover:bg-transparent hover:text-white ': 'text-white hover:bg-black/5 hover:text-black '" class="nav-link" to="/#services">Services</NuxtLink>
+      <NuxtLink :class="isSticky ? 'text-beige hover:bg-transparent hover:text-white ': 'text-white hover:bg-black/5 hover:text-black '" class="nav-link" to="/#services">Services</NuxtLink>
+      <NuxtLink :class="isSticky ? 'text-beige hover:bg-transparent hover:text-white ': 'text-white hover:bg-black/5 hover:text-black '" class="nav-link" to="/#services">Services</NuxtLink>
+     </nav>
+  </header>
+
+</template>
+
+<style scoped>
+@reference "@/assets/css/main.css";
+
+</style>
