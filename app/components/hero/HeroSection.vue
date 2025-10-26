64aa48b9d9efe70eb076b49ad71dcc5e72f0e333
mohamedDev69 <mohamedchettah0208@gmail.com>
2025-10-26 15:16:05 +0100

    feat: first commit

---
+++ app/components/hero/HeroSection.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section
+      class="relative flex items-center justify-center h-[600px] bg-cover bg-center"
+      style="background-image: url('/img/main/background.png');"
+  >
+    <div class="text-center text-white">
+      <h1 class="text-4xl font-bold mb-4">Ta headline ici</h1>
+      <button class="bg-blue-600 px-4 py-2 rounded">Call to Action</button>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
