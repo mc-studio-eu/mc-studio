0d3ca1bee590bd8ec2d6f4f4a0848f0a96c65bed
mohamedDev69 <mohamedchettah0208@gmail.com>
2025-10-26 20:36:10 +0100

    feat: wip navbar / hero section

--- app/components/hero/HeroSection.vue
+++ app/components/hero/HeroSection.vue
@@ -4,12 +4,12 @@ import Navbar from "../layouts/Navbar.vue";
 </script>
 
 <template>
-  <div class="h-[600px] relative bg-cover bg-center " style="background-image: url('/img/main/background.png');" >
+  <div class=" h-[600px] relative bg-cover bg-center " style="background-image: url('/img/main/background.png');" >
     <Navbar />
 
-    <div class="text-center text-white">
+    <div class="mt-26 text-center text-white">
       <h1 class="text-5xl mb-4" >On ne crée pas des sites. <br> On construit des expériences</h1>
-      <button class="bg-black-light px-4 py-2 rounded">Call to Action</button>
+
     </div>
   </div>
 </template>
--- app/components/layouts/Navbar.vue
+++ app/components/layouts/Navbar.vue
@@ -4,23 +4,44 @@ const isSticky = ref(false)
 
 <template>
   <!-- Header -->
-  <header class="flex justify-center items-center">
+  <header class="pt-10 flex justify-around items-center w-full">
     <!-- Logo -->
-    <NuxtLink to="/" class="mt-10 flex justify-center items-center">
-      <NuxtImg src="/img/main/logo.svg"  alt="MC Studio" class="h-16" />
-    </NuxtLink>
+    <div class="flex gap-2 items-center">
+      <NuxtLink to="/" class="flex justify-center items-center">
+        <NuxtImg src="/img/main/logo.svg"  alt="MC Studio" class="h-10" />
+      </NuxtLink>
+      <p class="text-white text-sm">Studio</p>
+    </div>
+
+
 
     <!-- Nav desktop -->
-    <nav class="hidden sm:flex items-center gap-2 sm:gap-4">
-      <NuxtLink :class="isSticky ? 'text-beige hover:bg-transparent hover:text-white ': 'text-white hover:bg-black/5 hover:text-black '" class="nav-link" to="/#services">Services</NuxtLink>
-      <NuxtLink :class="isSticky ? 'text-beige hover:bg-transparent hover:text-white ': 'text-white hover:bg-black/5 hover:text-black '" class="nav-link" to="/#services">Services</NuxtLink>
-      <NuxtLink :class="isSticky ? 'text-beige hover:bg-transparent hover:text-white ': 'text-white hover:bg-black/5 hover:text-black '" class="nav-link" to="/#services">Services</NuxtLink>
+    <nav class="flex items-center gap-2 sm:gap-4">
+      <NuxtLink :class="isSticky ? 'nav-sticky': 'nav-default'" class="nav-link" to="/#services">Projets</NuxtLink>
+      <NuxtLink :class="isSticky ? 'nav-sticky': 'nav-default'" class="nav-link" to="/#services">Services</NuxtLink>
+      <NuxtLink :class="isSticky ? 'nav-sticky': 'nav-default'" class="nav-link" to="/#services">Process</NuxtLink>
+      <NuxtLink :class="isSticky ? 'nav-sticky': 'nav-default'" class="nav-link" to="/#services">Avis</NuxtLink>
      </nav>
+
+    <!-- FR / EN -->
+    <UButton trailing-icon="i-lucide-arrow-right" size="md">Button</UButton>
   </header>
 
 </template>
 
 <style scoped>
 @reference "@/assets/css/main.css";
+
+.nav-link{
+  @apply  font-inter text-xs lg:px-2 py-1.5 rounded-xl transition-colors duration-200;
+}
+
+.nav-default{
+  @apply text-white hover:bg-black/5 hover:text-gold
+}
+
+.nav-sticky{
+  @apply text-gold hover:bg-transparent hover:text-white ;
+}
 
 </style>
